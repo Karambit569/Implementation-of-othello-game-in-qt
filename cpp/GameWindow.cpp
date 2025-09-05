b45da75e1b34b2dc75b74269fc782a583b83ff43
Karambit569 <diggertv569@gmail.com>
2025-09-06 03:51:24 +0900

    Update GameWindow.cpp

--- cpp/GameWindow.cpp
+++ cpp/GameWindow.cpp
@@ -6,24 +6,24 @@
 GameWindow::GameWindow(const GameSettingDialog::GameConfig &config, QWidget *parent)
     : QWidget(parent) {
 
-    // 게임판 위젯 생성
+    // Widget
     boardWidget = new GameBoardWidget(config, this);
 
-    // 라벨 생성
+    // Label
     turnLabel = new QLabel(this);
     blackCountLabel = new QLabel(this);
     whiteCountLabel = new QLabel(this);
 
-    // 라벨을 GameBoardWidget에도 전달
+    // GameBoardWidget
     boardWidget->turnLabel = turnLabel;
     boardWidget->blackCountLabel = blackCountLabel;
     boardWidget->whiteCountLabel = whiteCountLabel;
 
-    // 버튼 생성
+    // Buttons
     backButton = new QPushButton("메인화면으로", this);
     connect(backButton, &QPushButton::clicked, this, &GameWindow::goToMainMenu);
 
-    // 오른쪽 레이아웃 구성
+    // Layout
     QVBoxLayout *sideLayout = new QVBoxLayout;
     sideLayout->addWidget(turnLabel);
     sideLayout->addWidget(blackCountLabel);
@@ -31,18 +31,17 @@ GameWindow::GameWindow(const GameSettingDialog::GameConfig &config, QWidget *par
     sideLayout->addStretch();
     sideLayout->addWidget(backButton);
 
-    // 전체 레이아웃
+    // ovr layout
     QHBoxLayout *mainLayout = new QHBoxLayout(this);
-    mainLayout->addWidget(boardWidget, 1);       // 비율 1
+    mainLayout->addWidget(boardWidget, 1);       
     mainLayout->addLayout(sideLayout);
 
     setLayout(mainLayout);
-    setWindowTitle("오델로");
+    setWindowTitle("Othello");
     resize(800, 600);
 }
 
 void GameWindow::goToMainMenu() {
-    QMessageBox::information(this, "메인화면", "메인화면으로 돌아갑니다.");
-    // 여기서 메인 윈도우 호출 또는 현재 창 닫기 처리 가능
+    QMessageBox::information(this, "Main", "Exit to main");
     close();
 }
